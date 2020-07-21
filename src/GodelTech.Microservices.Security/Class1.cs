a505b979bb761a0d3f1931b35269e8def6fc6dbd
diamond_dragon <diamond_dragon@tut.by>
2020-07-21 23:38:45 +0300

    Initial commit

---
+++ src/GodelTech.Microservices.Security/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GodelTech.Microservices.Security
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/GodelTech.Microservices.Security.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GodelTech.Microservices.Security.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
