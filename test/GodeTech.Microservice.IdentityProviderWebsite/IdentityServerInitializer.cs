43b6cf28557ec28f58745c80f20fe560dee4ac21
diamond_dragon <diamond_dragon@tut.by>
2020-07-25 13:18:21 +0300

    Test identity server instance added

--- test/GodeTech.Microservice.IdentityProviderWebsite/IdentityServerInitializer.cs
+++ test/GodeTech.Microservice.IdentityProviderWebsite/IdentityServerInitializer.cs
@@ -6,6 +6,11 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace GodeTech.Microservice.IdentityProviderWebsite
 {
+    /// <summary>
+    /// This class is expected to be used for demo purposes only. More details can be found here:
+    /// https://github.com/IdentityServer/IdentityServer4.Quickstart.UI/tree/main
+    /// https://identityserver4.readthedocs.io/en/latest/quickstarts/2_interactive_aspnetcore.html
+    /// </summary>
     public class IdentityServerInitializer : MicroserviceInitializerBase
     {
         public IdentityServerInitializer(IConfiguration configuration) 
@@ -15,9 +20,12 @@ namespace GodeTech.Microservice.IdentityProviderWebsite
 
         public override void ConfigureServices(IServiceCollection services)
         {
+
             var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients);
+                .AddInMemoryClients(Config.Clients)
+                .AddTestUsers(Config.Users);
 
             builder.AddDeveloperSigningCredential();
         }
--- test/GodeTech.Microservice.IdentityProviderWebsite/Quickstart/TestUsers.cs
+++ test/GodeTech.Microservice.IdentityProviderWebsite/Quickstart/TestUsers.cs
@@ -1,65 +1,18 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Text.Json;
-using IdentityModel;
 using IdentityServer4;
-using IdentityServer4.Models;
-using IdentityServer4.Test;
 
-namespace GodeTech.Microservice.IdentityProviderWebsite
+namespace IdentityServerHost.Quickstart.UI
 {
-    public static class Config
+    public class TestUsers
     {
-        public static IEnumerable<IdentityResource> IdentityResources =
-            new List<IdentityResource>
-            {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-            };
-
-
-        public static IEnumerable<ApiScope> ApiScopes =
-            new List<ApiScope>
-            {
-                new ApiScope("api1", "My API")
-            };
-
-        public static IEnumerable<Client> Clients =
-            new List<Client>
-            {
-                // machine to machine client
-                new Client
-                {
-                    ClientId = "client",
-                    ClientSecrets = { new Secret("secret".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    // scopes that client has access to
-                    AllowedScopes = { "api1" }
-                },
-                
-                // interactive ASP.NET Core MVC client
-                new Client
-                {
-                    ClientId = "mvc",
-                    ClientSecrets = { new Secret("secret".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-                    
-                    // where to redirect to after login
-                    RedirectUris = { "https://localhost:5002/signin-oidc" },
-
-                    // where to redirect to after logout
-                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
-
-                    AllowedScopes = new List<string>
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile
-                    }
-                }
-            };
-
         public static List<TestUser> Users
         {
             get
@@ -71,7 +24,7 @@ namespace GodeTech.Microservice.IdentityProviderWebsite
                     postal_code = 69118,
                     country = "Germany"
                 };
-
+                
                 return new List<TestUser>
                 {
                     new TestUser
@@ -109,6 +62,5 @@ namespace GodeTech.Microservice.IdentityProviderWebsite
                 };
             }
         }
-
     }
 }
