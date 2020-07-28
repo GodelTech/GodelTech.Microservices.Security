f2662007804d0830e242807a458a683e2d3de175
diamond_dragon <diamond_dragon@tut.by>
2020-07-29 00:06:29 +0300

    UI test project was added

---
+++ test/GodelTech.Microservices.UiWebsite/Startup.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using GodelTech.Microservices.Core;
+using GodelTech.Microservices.Core.Mvc;
+using GodelTech.Microservices.Security;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace GodelTech.Microservices.UiWebsite
+{
+    public class Startup : MicroserviceStartup
+    {
+        public Startup(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+
+        protected override IEnumerable<IMicroserviceInitializer> CreateInitializers()
+        {
+            yield return new DeveloperExceptionPageInitializer(Configuration)
+            {
+                ErrorHandlingPath = "/Error"
+            };
+            
+            yield return new GenericInitializer((app, env) => app.UseStaticFiles());
+            yield return new GenericInitializer((app, env) => app.UseRouting());
+            
+            //yield return new UiSecurityInitializer(Configuration);
+            yield return new RazorPagesInitializer(Configuration);
+        }
+    }
+}
