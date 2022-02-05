16f7eda946adb8012cf905f5939c9b46e4b88591
Andrei Yaskevich <andrez.yaskevich@gmail.com>
2022-02-05 18:33:36 +0300

    add openFakeControler tests

--- test/GodelTech.Microservices.Security.IntegrationTests/ApiSecurityInitializerTests.cs
+++ test/GodelTech.Microservices.Security.IntegrationTests/ApiSecurityInitializerTests.cs
@@ -12,7 +12,6 @@ using Xunit;
 
 namespace GodelTech.Microservices.Security.IntegrationTests
 {
-    // todo: write tests for openFake method without security
     // todo: add UseHsts for demo projects???
     [Collection("TestCollection")]
     public sealed class ApiSecurityInitializerTests : IDisposable
@@ -179,6 +178,54 @@ namespace GodelTech.Microservices.Security.IntegrationTests
 
             // Assert
             Assert.Equal(HttpStatusCode.Forbidden, result.StatusCode);
+        }
+
+        public static IEnumerable<object[]> OpenFakeRequestMemberData =>
+            new Collection<object[]>
+            {
+                new object[]
+                {
+                    string.Empty,
+                    new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Get,
+                        RequestUri = new Uri("openFakes", UriKind.Relative)
+                    },
+                    HttpStatusCode.OK
+                }
+            };
+
+        [Theory]
+        [MemberData(nameof(OpenFakeRequestMemberData))]
+        public async Task UnsecuredEndpointRequested_WhenJwtTokenProvided_ReturnsCorrectStatusCode(
+            string scope,
+            HttpRequestMessage httpRequestMessage,
+            HttpStatusCode expectedResponseCode)
+        {
+            // Arrange 
+            var token = await _fixture.TokenService.GetClientCredentialsTokenAsync("ClientForApi", "secret", scope);
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            // Act
+            var result = await _httpClient.SendAsync(httpRequestMessage);
+
+            // Assert
+            Assert.Equal(expectedResponseCode, result.StatusCode);
+        }
+
+        [Theory]
+        [MemberData(nameof(OpenFakeRequestMemberData))]
+        public async Task UnsecuredEndpointRequested_WhenJwtTokenNotProvided_ReturnsCorrectStatusCode(
+            string scope,
+            HttpRequestMessage httpRequestMessage,
+            HttpStatusCode expectedResponseCode)
+        {
+            // Arrange & Act
+            var result = await _httpClient.SendAsync(httpRequestMessage);
+
+            // Assert
+            Assert.Equal(expectedResponseCode, result.StatusCode);
         }
     }
 }
