6ca99b5fb0e6c6cca208114948b45703c5795fef
Vladimir Rodchenko <vladimir@rodchenko.by>
2022-02-05 18:59:58 +0300

    solve ProjectHelpers using GodelTech.Extensions.Configuration

--- test/GodelTech.Microservices.Security.IntegrationTests/Applications/ApplicationBase.cs
+++ test/GodelTech.Microservices.Security.IntegrationTests/Applications/ApplicationBase.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
-using GodelTech.Microservices.Security.IntegrationTests.Fakes;
+using GodelTech.Extensions.Configuration;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
