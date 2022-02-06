c69880daf6de1bb13bc926423f26c55792f0077d
Vladimir Rodchenko <vladimir@rodchenko.by>
2022-02-06 17:17:22 +0300

    add unit test to check code coverage

---
+++ test/GodelTech.Microservices.Security.Tests/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace GodelTech.Microservices.Security.Tests
+{
+    // todo: solve this
+    public class UnitTest
+    {
+        [Fact]
+        public void A()
+        {
+            Assert.True(true);
+        }
+    }
+}
