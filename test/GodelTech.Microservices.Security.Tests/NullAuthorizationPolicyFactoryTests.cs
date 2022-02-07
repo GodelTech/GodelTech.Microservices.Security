8db6530a569c51971aa5e9f1fe073833c7a15b54
Andrei Yaskevich <andrez.yaskevich@gmail.com>
2022-02-07 11:29:33 +0300

    add NullAuthorizationPolicyFactory tests

---
+++ test/GodelTech.Microservices.Security.Tests/NullAuthorizationPolicyFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.AspNetCore.Authorization;
+using Xunit;
+
+namespace GodelTech.Microservices.Security.Tests
+{
+    public class NullAuthorizationPolicyFactoryTests
+    {
+        [Fact]
+        public void Create_Success()
+        {
+            // Arrange
+            var policyFactory = new NullAuthorizationPolicyFactory();
+
+            // Act
+            var result = policyFactory.Create();
+
+            // Assert
+            result.Should().BeEquivalentTo(new Dictionary<string, AuthorizationPolicy>());
+        }
+    }
+}
