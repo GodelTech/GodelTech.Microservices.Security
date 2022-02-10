9d47642bb79942db0062ad44663824dc241cb750
Andrei Yaskevich <andrez.yaskevich@gmail.com>
2022-02-10 12:00:33 +0300

    add urihelpers tests

---
+++ test/GodelTech.Microservices.Security.Tests/Helpers/UrlHelpersTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using GodelTech.Microservices.Security.Helpers;
+using Xunit;
+
+namespace GodelTech.Microservices.Security.Tests.Helpers
+{
+    public class UrlHelpersTests
+    {
+        [Fact]
+        public void ChangeAuthority_WhenPublicAuthorityUriIsNull_Success()
+        {
+            // Arrange & Act
+            var result = UrlHelpers.ChangeAuthority("Test Authority", null);
+
+            // Assert
+            Assert.Equal("Test Authority", result);
+        }
+
+        [Fact]
+        public void ChangeAuthority_WhenPublicAuthorityUri_Success()
+        {
+            // Arrange
+            var publicAuthorityUri = new Uri("https://test.publicauthority");
+
+            // Act
+            var result = UrlHelpers.ChangeAuthority("https://test.authority/testAbsoluteUri", publicAuthorityUri);
+
+            // Assert
+            Assert.Equal("https://test.publicauthority/testAbsoluteUri", result);
+        }
+    }
+}
