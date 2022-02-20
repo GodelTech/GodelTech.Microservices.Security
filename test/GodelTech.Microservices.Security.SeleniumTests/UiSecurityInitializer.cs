fd0273bec4e83ab6deb81b0bfd23308fdc09f51f
Andrei Yaskevich <andrez.yaskevich@gmail.com>
2022-02-20 12:01:58 +0300

    rename test

--- test/GodelTech.Microservices.Security.SeleniumTests/UiSecurityInitializer.cs
+++ test/GodelTech.Microservices.Security.SeleniumTests/UiSecurityInitializer.cs
@@ -1,6 +1,7 @@
 using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using Xunit;
