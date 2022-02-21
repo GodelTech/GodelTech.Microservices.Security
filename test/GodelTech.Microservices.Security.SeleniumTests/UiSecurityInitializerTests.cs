67ea5845d42aada72d6050afcb7617fbab530ebc
Andrei Yaskevich <andrez.yaskevich@gmail.com>
2022-02-21 08:55:21 +0300

    clear usings and rename tests

--- test/GodelTech.Microservices.Security.SeleniumTests/UiSecurityInitializerTests.cs
+++ test/GodelTech.Microservices.Security.SeleniumTests/UiSecurityInitializerTests.cs
@@ -1,7 +1,6 @@
 using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using Xunit;
